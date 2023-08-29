e476b1921cbac22da72a814c7c6574dce836717c
Matt Naslcheraghi <63419934+mansnc@users.noreply.github.com>
2023-08-29 14:26:50 -0400

    Following options added to the ping command: - TTL - Packet size - Interval
    Multi-packet echo is also modified to comply with the new changes.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from module_latency_measurement import measure_latency
 
-ip_addr = "8.8.8.8"
-pkts2send = "4"
-latency = measure_latency(ip_addr, pkts2send)
+addr = "8.8.8.8"
+pkts2send = 5
+pktsize = 64
+ttl = 128
+interval = 1
+IP_v = 4 # or 6
+
+latency = measure_latency(addr, IP_v, pkts2send, pktsize, ttl, interval)
 
 if latency is not None:
-    print(f"Latency to {ip_addr}: {latency} ms")
+    print(f"Latency to {addr}: {latency} ms")
 else:
-    print(f"Failed to measure latency to {ip_addr}")
+    print(f"Failed to measure latency to {addr}")
